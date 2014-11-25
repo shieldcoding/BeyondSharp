13e26b76dc399d5d404d9e6825e81e6b8769b4a5
Richard Lang <richard@shieldcoding.com>
2014-11-25 12:16:48 -0500

    Start of the framework engine. Server will use the command line parser.

---
+++ BeyondSharp Common/FrameworkEngine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeyondSharp.Common
+{
+    /// <summary>
+    /// Represents a sided instance of the BeyondSharp engine.
+    /// </summary>
+    public abstract class FrameworkEngine
+    {
+        /// <summary>
+        /// The sided (client/server) context of the engine.
+        /// </summary>
+        public FrameworkEngineSide Side { get; protected set; }
+    }
+}
---
+++ BeyondSharp Server/ServerProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeyondSharp.Server
+{
+    public static class ServerProgram
+    {
+        public static ServerFrameworkEngine Engine { get; private set; }
+
+        public ServerProgramOptions Options { get; private set; }
+
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
