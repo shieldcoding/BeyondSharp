c631ebc80c750c8c76c0f13180fdd284ee0740a5
Richard Lang <richard@shieldcoding.com>
2014-11-25 13:11:00 -0500

    Initial framework structure

---
+++ BeyondSharp Client/Framework/ClientMovable.cs
@@ -0,0 +1,13 @@
+﻿using BeyondSharp.Common.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeyondSharp.Client.Framework
+{
+    internal class ClientMovable : ClientAtom, ICommonMovable
+    {
+    }
+}
