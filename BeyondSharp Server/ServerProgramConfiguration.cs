785e62d71894431b5c728c9b73c3bae39810fb8e
Richard Lang <richard@shieldcoding.com>
2014-11-26 16:50:16 -0500

    Added Json.net for initially handling configuration options.

---
+++ BeyondSharp Server/ServerProgramConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeyondSharp.Server
+{
+    public class ServerProgramConfiguration
+    {
+        public NetworkConfiguration Network { get; private set; }
+
+        public class NetworkConfiguration
+        {
+            [JsonProperty]
+            public IPAddress Address { get; set; }
+
+            [JsonProperty]
+            [DefaultValue(7777)]
+            public int Port { get; set; }
+
+            public NetworkConfiguration()
+            {
+                Address = IPAddress.Any;
+            }
+        }
+    }
+}
