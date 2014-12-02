3e01d462a90cf7bb479a5430b960e834a86cdfc4
Richard Lang <richard@shieldcoding.com>
2014-12-01 21:17:34 -0500

    Working on entity components and networked images.

--- BeyondSharp Common/Entity/ICommonEntityComponent.cs
+++ BeyondSharp Common/Entity/ICommonEntityComponent.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BeyondSharp.Common.Entity.Component.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ BeyondSharp Server/Graphics/ServerIcon.cs
@@ -0,0 +1,13 @@
+﻿using BeyondSharp.Common.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeyondSharp.Server.Graphics
+{
+    public class ServerIcon : IIcon
+    {
+    }
+}
