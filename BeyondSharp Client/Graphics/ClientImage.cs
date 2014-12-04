e63d0130361e947547d8c96372df8df9ad6a2eb8
Richard Lang <richard@shieldcoding.com>
2014-12-04 09:55:57 -0500

    Added some documentation for the client side of images, made image operations serializable.

--- BeyondSharp Client/Graphics/ClientImage.cs
+++ BeyondSharp Client/Graphics/ClientImage.cs
@@ -10,16 +10,33 @@ namespace BeyondSharp.Client.Graphics
 {
     public class ClientImage : Image<ClientImage>
     {
+        /// <summary>
+        /// Applies an image blend operation to the source image using the supplied image via the supplied method.
+        /// </summary>
+        /// <param name="image">The image to be blended into the source image.</param>
+        /// <param name="method">The method by which to blend the two images.</param>
+        /// <returns>The client image with the image operation applied.</returns>
         public override ClientImage Blend(ClientImage image, ImageBlendMethod method)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Applies a color blend operation to the source image using the supplied color via the supplied method.
+        /// </summary>
+        /// <param name="color">The color to be blended into the source image.</param>
+        /// <param name="method">The method by which to blend the two images.</param>
+        /// <returns>The client image with the image operation applied.</returns>
         public override ClientImage Blend(Color color, ImageBlendMethod method)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Applies a crop operation to the source image using the supplied area.
+        /// </summary>
+        /// <param name="area">The area to be cropped from the source image.</param>
+        /// <returns>The client image with the image operation applied.</returns>
         public override ClientImage Crop(Rectangle area)
         {
             throw new NotImplementedException();
--- BeyondSharp Common/Graphics/ImageOperation.cs
+++ BeyondSharp Common/Graphics/ImageOperation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -13,14 +14,20 @@ namespace BeyondSharp.Common.Graphics
     public abstract class ImageOperation<ImageType>
         where ImageType : Image<ImageType>
     {
+        [JsonProperty]
+        public ImageOperationType OperationType { get; private set; }
+
         public class ImageBlendOperation : ImageOperation<ImageType>
         {
+            [JsonProperty]
             public ImageType Image { get; private set; }
 
+            [JsonProperty]
             public ImageBlendMethod Method { get; private set; }
 
             public ImageBlendOperation(ImageType image, ImageBlendMethod method)
             {
+                OperationType = ImageOperationType.ImageBlend;
                 Image = image;
                 Method = method;
             }
@@ -28,12 +35,15 @@ namespace BeyondSharp.Common.Graphics
 
         public class ColorBlendOperation : ImageOperation<ImageType>
         {
+            [JsonProperty]
             public Color Color { get; private set; }
 
+            [JsonProperty]
             public ImageBlendMethod Method { get; private set; }
 
             public ColorBlendOperation(Color color, ImageBlendMethod method)
             {
+                OperationType = ImageOperationType.ColorBlend;
                 Color = color;
                 Method = method;
             }
@@ -41,12 +51,21 @@ namespace BeyondSharp.Common.Graphics
 
         public class CropOperation : ImageOperation<ImageType>
         {
+            [JsonProperty]
             public Rectangle Area { get; private set; }
 
             public CropOperation(Rectangle area)
             {
+                OperationType = ImageOperationType.Crop;
                 Area = area;
             }
+        }
+
+        public enum ImageOperationType
+        {
+            ImageBlend,
+            ColorBlend,
+            Crop
         }
     }
 }
